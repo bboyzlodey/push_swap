256cc86a1c7f0f7558ec71972fd76f4494fbc47a
Denis Skarlat <denis.skarlat@quantron-systems.com>
2020-10-15 01:35:08 +0300

    Fix infinite cycle after rotate() call

--- instructions.c
+++ instructions.c
@@ -10,12 +10,19 @@ void	swap(stack *swap)
 	t_item *bellow_top;
 
 	bellow_top = NULL;
-	if (swap->stack_size > 1)
+	if (swap->stack_size > 2)
 	{
 		bellow_top = swap->head->next;
 		swap->head->next = bellow_top->next;
 		bellow_top->next = swap->head;
 		swap->head = bellow_top;
+	}
+	else if (swap->stack_size == 2)
+	{
+		swap->foots = swap->head;
+		swap->head = swap->foots->next;
+		swap->head->next = swap->foots;
+		swap->foots->next = NULL;
 	}
 }
 
@@ -25,6 +32,8 @@ void	rotate(stack *rotate)
 
 	prelast = NULL;
 	prelast = rotate->head;
+	if (rotate->stack_size < 2)
+		return ;
 	while (prelast->next && prelast->next != rotate->foots)
 	{
 		prelast = prelast->next;
