826fe8edb08cade9bc04735e632ef60082c22272
Denis Skarlat <denis.skarlat@quantron-systems.com>
2020-10-15 00:12:39 +0300

    Add check of valid args

--- item_utils.c
+++ item_utils.c
@@ -1,20 +1,34 @@
 #include "push_swap.h"
 
-int		init_item(t_item *item, char *value)
+static int	additional_validation(char *valid)
+{
+	if (valid[0] == 0)
+	{
+		return 0;
+	}
+	else if (!ft_isdigit(*valid))
+	{
+		return 1;
+	}
+	return additional_validation(valid + 1);
+}
+
+int			init_item(t_item *item, char *value)
 {
 	item->next = NULL;
 	item->prev = NULL;
 	item->value = 0;
 	if (value == NULL || value[0] == '\0')
 		return -1;
-	else if ((item->value = ft_atoi(value)) == 0 && value[0] == '0' && value[1] != '\0' || (value[0] != '0' && item->value == 0))
+	else if (additional_validation(value))
 	{
 		return -1;
 	}
+	item->value = ft_atoi(value);
 	return 0;	
 }
 
-t_item	*item_from_int(int val)
+t_item		*item_from_int(int val)
 {
 	t_item	*tmp;
 
@@ -29,7 +43,7 @@ t_item	*item_from_int(int val)
 	return tmp;
 }
 
-t_item	*item_malloc(void)
+t_item		*item_malloc(void)
 {
 	return (t_item*)ft_memalloc(sizeof(t_item));
 }
